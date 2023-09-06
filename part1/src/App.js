95ad629c316766032017208008edc67872c8a843
gerry <13632429781@163.com>
2023-09-07 00:17:13 +0800

    feat(javascript): 回顾js结合react

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,6 +1,6 @@
 //import logo from './logo.svg';
 //import './App.css';
-
+import MyComponent from './part1/MyComponent.js';
 // function App() {
 //   return (
 //     <div className="App">
@@ -28,12 +28,149 @@ const Hello = (props) =>{
     </div>
   )
 }
+const Jsvar = () => {
+  const x = 1
+  let y = 5
+  //console.log(x,y)
+  y += 10
+  console.log(x,y)
+  y = 'test1'
+  console.log(x,y)
+}
+
+const Jsarr = () => {
+  const t = [1,-1,3]
+  t.push(5)
+
+  //console.log(t.length)
+  //console.log(t[1])
+
+  t.forEach(value => {
+    //console.log(value)
+  })
+
+  //const t2 = t.concat(5)
+  //console.log(t)
+  //console.log(t2)
+
+  //const m1 = t.map(value => value * 2)
+  //console.log(m1)
+
+  const [first,second,...rest] = t
+  console.log(first,second)
+  console.log(rest)
+}
+
+const Jsobj = () => {
+  const object1 = {
+    name :'huxxhua',
+    age : 30,
+    size: 20,
+  }
+
+  const object2 = {
+    name : {
+      first:'huang',
+      last:'guanhua'
+    },
+    grades : [2,3,4,9]
+  }
+
+  object2.address = '东莞市'
+
+  console.log(object2.grades,object2.name,object2.address)
+}
+
+const Jsfun = () => {
+  const sum = (p1,p2) => {
+    //console.log(p1)
+    //console.log(p2)
+    return p1 + p2;
+  }
+  const result = sum(1,5)
+  //console.log(result)
+
+  const square = p =>{
+    return p*p
+  }
+
+  const square_concise = p => p*p
+
+  const t = [1,2,3]
+  const tsquare = t.map(p => p*p)
+  //console.log(tsquare)
+}
+
+const Header = (props) => {
+  console.log(props)
+  return <h1>{props.course}</h1>
+}
+
+const arto = {
+  name :'ci',
+  age : 18,
+  education : 'phd',
+  greet : function(){
+    //console.log('hello, my name is ' + this.age)
+  },
+  doAddition : function (a,b) {
+    //console.log(a + b)
+  }
+}
+
+arto.doAddition(1,4)
+const referenceToAddition = arto.doAddition
+//referenceToAddition(10,15)
+
+arto.grow = function(){
+  this.age +=1
+}
+//console.log(arto.age)
+
+//setTimeout(arto.greet.bind(arto),1000)
+//arto.greet()
+//const referenceToGreet = arto.greet
+//referenceToGreet()
+
+class Person{
+  constructor(name,age){
+    this.name = name
+    this.age = age
+  }
+  greet(){
+    //console.log('hello, my name is ' + this.name)
+  }
+}
+
+const me = new Person('ci',18)
+//me.greet()
 
 const App = () => {
+
+  //arto.greet()
   //console.log('Hello from component')
-  //const now = new Date()
+  // const course = {
+  //   name : 'Half Stack application development',
+  //   parts : [
+  //     {
+  //       name : 'Fundamentals of React',
+  //       exercises : 10
+  //     },
+  //     {
+  //       name : 'Using props to pass data',
+  //       exercises: 7
+  //     },
+  //     {
+  //       name : 'State of component',
+  //       exercises : 14
+  //     }
+  //   ]
+  // }
+  
   return (
-    <><h1>huxxhux</h1><Hello name="xiaoming" age={13 + 12} /><Hello name="ci" /></>
+    <div>
+      ...
+    </div>
   )
 }
 export default App;
---
+++ part1/src/part1/MyComponent.js
@@ -0,0 +1,7 @@
+const content = (props) =>{
+    return (
+      <div>
+        <p>Hello {props.name},you are {props.age} years old</p>
+      </div>
+    )
+  }
